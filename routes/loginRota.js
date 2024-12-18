d19a62973b5792737d3023da8a8b7cfd29f93743
Brenda Cristaldo <112977198+brendacristaldo@users.noreply.github.com>
2024-12-18 19:51:24 -0300

    swagger update

--- routes/loginRota.js
+++ routes/loginRota.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
-const jwt = require('jsonwebtoken');
-const bcrypt = require('bcryptjs');
+//const jwt = require('jsonwebtoken');
+//const bcrypt = require('bcryptjs');
 const { usuarios } = require('../dados/usuarios');
 const crypto = require('crypto');
 
---
+++ swagger.js
@@ -0,0 +1,5 @@
+const swaggerAutogen = require("swagger-autogen")();
+output = "./swagger_doc.json";
+endpoints = []
+
+swaggerAutogen(output, endpoints);
