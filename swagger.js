34eb5fdb673c4804470fda0576308f634cb88b67
Nathália <miyagunimiyuki@gmail.com>
2024-12-19 15:50:10 -0300

    Update swagger.js

--- swagger.js
+++ swagger.js
@@ -1,9 +1,11 @@
 const swaggerAutogen = require("swagger-autogen")();
 output = "./swagger_doc.json";
-endpoints = [
-
-
+(endpoints = [
+    "./routes/adminRota.js",
+    "./routes/loginRota.js",
+    "./routes/perfilRota.js",
+    "./routes/usuarioExcluirRota.js",
     
-]
+  ]),
 
 swaggerAutogen(output, endpoints);
